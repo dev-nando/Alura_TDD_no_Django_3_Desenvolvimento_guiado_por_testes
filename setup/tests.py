2d5fe32187088fac7e326b846c12d714c73d1e30
fernando <fcechett@gmail.com>
2021-06-09 17:42:25 -0300

    2.3 - URL do servidor e teste falha

--- setup/tests.py
+++ setup/tests.py
@@ -4,8 +4,15 @@ from selenium import webdriver
 class AnimaisTestCase(LiveServerTestCase):
     
     def setUp(self):
-        self.browser = webdriver.Chrome('..\chromedriver.exe')
+        self.browser = webdriver.Chrome('.\chromedriver.exe')
         
     def tearDown(self):
         self.browser.quit()
+    
+    def test_abre_janela_do_chrome(self):
+        self.browser.get(self.live_server_url)
+        
+    def test_deu_ruim(self):
+        """teste de exemplo de erro"""
+        self.fail("Teste falhou, deu ruim mesmo!")
         
