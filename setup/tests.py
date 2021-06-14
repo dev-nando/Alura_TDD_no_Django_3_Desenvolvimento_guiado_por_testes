4e5815264ad7877c24a5ccc4b2c51f0a1aa13cd9
fernando <fcechett@gmail.com>
2021-06-14 10:45:27 -0300

    4.2 - Testando campo de busca

--- setup/tests.py
+++ setup/tests.py
@@ -22,7 +22,8 @@ class AnimaisTestCase(LiveServerTestCase):
         brand_element = self.browser.find_element_by_css_selector('.navbar')
         self.assertEqual('Busca Animal', brand_element.text)
         # Ele vê um campo para pesquisar animais pelo nome. 
-        
+        buscar_animal_input = self.browser.find_element_by_css_selector('input#buscar-animal')
+        self.assertEqual(buscar_animal_input.get_attribute('placeholder'), 'Exemplo: leão, urso...')
         # Ele pesquisa por Leão e clica no botão pesquisar.
 
         # O site exibe 4 caracteristicas do animal pesquisado.
