fe685a036554c57a2950c1fda683273a7c586ef1
fernando <fcechett@gmail.com>
2021-06-14 14:05:40 -0300

    6.4 - Realizando buscas

--- animais/views.py
+++ animais/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from animais.models import Animal
 
 def index(request):
-    context = {'caracteristicas': None}
-    
+    context = {'caracteristicas' : None}
+
     if 'buscar' in request.GET:
         animais = Animal.objects.all()
         animal_pesquisado = request.GET['buscar']
